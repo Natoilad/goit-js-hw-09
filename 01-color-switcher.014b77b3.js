90b023af0972f0af40e9d8bae40c7bbb48b72aae
Natoilad <109770565+Natoilad@users.noreply.github.com>
2022-12-25 14:19:30 +0000

    Deploying to gh-pages from @ Natoilad/goit-js-hw-09@b26fc89009d4d7a7732751bf5b25d2afa8ba1e60 🚀

---
+++ 01-color-switcher.014b77b3.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.disabled=!0;var n=document.querySelector("body");t.addEventListener("click",(function(){e.disabled=!1,t.disabled=!0,a=setInterval((function(){n.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,clearInterval(a)}));var a=null}();
+//# sourceMappingURL=01-color-switcher.014b77b3.js.map
