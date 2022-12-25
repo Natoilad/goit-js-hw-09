b26fc89009d4d7a7732751bf5b25d2afa8ba1e60
Natoilad <109770565+Natoilad@users.noreply.github.com>
2022-12-25 16:19:00 +0200

    task 1 complete

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+// Кнопки
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+btnStop.disabled = true;
+const bodyEl = document.querySelector('body');
 
+// Слухачі
+
+btnStart.addEventListener('click', startChangeBgColor);
+btnStop.addEventListener('click', stopChangeBgColor);
+
+let timerIdInterval = null;
+
+// Функції
+
+function randomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function startChangeBgColor() {
+  btnStop.disabled = false;
+  btnStart.disabled = true;
+  timerIdInterval = setInterval(() => {
+    bodyEl.style.background = randomHexColor();
+  }, 1000);
+}
+
+function stopChangeBgColor() {
+  btnStop.disabled = true;
+  btnStart.disabled = false;
+  clearInterval(timerIdInterval);
+}
