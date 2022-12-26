5e88bc5907885530a341210fee3cde9d519f0792
Natoilad <109770565+Natoilad@users.noreply.github.com>
2022-12-27 01:06:41 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
+const inputNumberDelay = document.querySelector(`input[name='delay']`);
+const inputNumberStep = document.querySelector(`input[name='step']`);
+const inputNumberAmount = document.querySelector(`input[name='amount']`);
+const buttonCreateProm = document.querySelector('button');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+buttonCreateProm.addEventListener('click', createWhenSubmit);
+
+function createWhenSubmit(e) {
+  e.preventDefault();
+  const onePromDelay = +inputNumberDelay.value;
+  const stepProm = +inputNumberStep.value;
+  const amountProm = +inputNumberAmount.value;
+  for (let i = 0; i <= amountProm; i += 1) {
+    const delay = onePromDelay + i * stepProm;
+    createPromise(i + 1, delay),
+      then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      }).catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
+  form.reset();
 }
